da01f153c03203e81b0172bfcd0fb3709a569398
shevkaboss <hikeqq@gmail.com>
2020-04-21 19:30:54 +0300

    no message

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Question extends Component{
     render(){
         var questionBlocks = this.props.questionData.questionBlocks.map(element => {
-            return <div key={element.text}>{element.text}</div>;
+            return (
+            <div key={element.text} className='question-block' dangerouslySetInnerHTML = {{__html:element.text}}></div>     
+            );
         })  
         return(
-            <div className='questionBlocks'>
+            <div className='question-blocks'>
                 <h2>{this.props.questionData.title}</h2>
                 {questionBlocks}
             </div>
